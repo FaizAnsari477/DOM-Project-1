d4f70452dbf782d2a8b60968062c951663e0be2a
FaizAnsari477 <faizansari477@gmail.com>
2024-02-27 15:14:07 +0530

    Added DOM Project

---
+++ script.js
@@ -0,0 +1,20 @@
+let body = document.getElementsByTagName("body")
+console.log(body)
+
+let div1 = document.getElementById("parent")
+console.log(div1)
+
+div1.style.backgroundColor = "green"
+div1.style.width = "550px";
+div1.style.height = "300px";
+div1.style.color = "yellow";
+div1.style.margin = "20px";
+
+let h1 = document.getElementById("heading");
+console.log(h1);
+
+h1.style.padding = "10px";
+h1.style.fontSize = "24px";
+h1.style.fontWeight = "bold";
+
+
